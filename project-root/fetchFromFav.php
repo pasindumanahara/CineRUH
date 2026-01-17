e27a6783e03a44173fa1d03b3d4cc8b4ce5d4728
Pasindu Manahara <manahara2003@gmail.com>
2026-01-17 17:27:33 +0530

    Refactor email retrieval and add comments
    Updated email retrieval to use session data instead of a hardcoded value. Added comments for clarity.

--- project-root/fetchFromFav.php
+++ project-root/fetchFromFav.php
@@ -1,4 +1,5 @@
 <?php 
+    // TODO :: this file should run with list fav button 
     require_once 'db.php';
 
     session_start();
@@ -8,12 +9,16 @@
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Allow-Headers: Content-Type");
 
-    //$email = $_SESSION['email'];
-    $email = "test@gmail.com";
+    // Email from server session data
+    $email = $_SESSION['email'];
+
+    // Get the keys from db , related to user email
     $query = "SELECT keys FROM fav_list WHERE email = '$email'";
 
+    // Establish connection and fetch resul into result
     $result = mysqli_query($conn, $query);
 
+    // Empty list
     if (mysqli_num_rows($result) === 0) {
         echo json_encode([
             "status" => "success",
@@ -24,11 +29,12 @@
 
 
     $keys = [];
-
+    // Fetch each line and to json obj
     while ($row = mysqli_fetch_assoc($result)) {
         $keys[] = $row['keys'];
     }
 
+    // Success --> return keys to fronend
     echo json_encode([
         "status" => "success",
         "key_list" => $keys
